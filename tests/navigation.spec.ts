df3831e8d056eecc74f33a63d16862cfed75fdf9
Barry Horgan <horgan.barry@gmail.com>
2026-03-22 19:34:04 +1100

    updated locator

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -16,9 +16,10 @@ test('User can navigate to form Layouts and see Inline form', async ({ page }) =
   await expect(inlineFormHeader).toBeVisible();
   
   //5. interaction: type into an input to prove the page is responsive
-/*   const emailInput = page.locator('input [placeholder="Email"]').first();
+    //const emailInput = page.locator('input [placeholder="Email"]').first();
+  const emailInput = await page.getByPlaceholder('Email').first();
   
   await emailInput.fill('barry@test.com.au');
   
-  await expect(emailInput).toHaveValue('barry@test.com.au'); */
+  await expect(emailInput).toHaveValue('barry@test.com.au');
 });
